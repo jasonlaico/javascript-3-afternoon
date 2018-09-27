703f12ed0d1b052f2b31c88cbd5352d02f91f0d8
Jason <42850772+jasonlaico@users.noreply.github.com>
2018-09-26 20:46:14 -0500

    done

--- classes.js
+++ classes.js
@@ -30,78 +30,115 @@
 */
 
 //Code Here
+class Employee{
+   constructor(first_name, last_name, email, age){
+     this.first_name = first_name;
+     this.last_name = last_name;
+     this.email = email;
+     this.age = age;
+     this.makeWidget = function(){
+      return first_name + ' ' + last_name + ' Widget';
+}}
+}var user01 = new Employee('Jason', 'L','jasonl@gmail.com',13);
+
+// ////////// PROBLEM 2 //////////
+
+// /*
+//   Next, make a manager for Widget Co.
+//   The manager has all the same properties as an Employee. Copy the Employee class and rename it Manager.
+//   Each manager has the following additional properties:
+//     - reports (other employees) that defaults to an empty array
+//   Each manager has the following additional methods:
+//     - hire (employee)
+//       - Accepts a new employee as a parameter and pushes it to their list of reports.
+//     - fire (index)
+//       - Fire removes employees from their list of reports at the given index
+
+//   Call your new class Manager
+// */
+// constructor(first_name, last_name, email, age){
+
+// //Code Here
+ 
+
+ 
+
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+    
+  }
+  
 
 
-
-////////// PROBLEM 2 //////////
-
-/*
-  Next, make a manager for Widget Co.
-  The manager has all the same properties as an Employee. Copy the Employee class and rename it Manager.
-  Each manager has the following additional properties:
-    - reports (other employees) that defaults to an empty array
-  Each manager has the following additional methods:
-    - hire (employee)
-      - Accepts a new employee as a parameter and pushes it to their list of reports.
-    - fire (index)
-      - Fire removes employees from their list of reports at the given index
-
-  Call your new class Manager
-*/
-
-//Code Here
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index,1)
+  }
+}
 
 
-
-////////// PROBLEM 3 //////////
-
-/*
-  Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  Progressive managers have all the same properties as managers. Copy the Manager class and rename
-  it to ProgressiveManager. Add the following additional properties:
-    - title - default 'Not a manager'
-    - bonus - default 0
-
-  When employees are hired or fired, the manager's title should be updated based on the number of reports.
-    0 reports : Not a manager
-    1-3 reports : Barely Manager
-    4-10 reports : Mostly Manager
-    11-50 reports : Manager
-    51-100 reports : Manager Plus
-    101+ reports : Bestest Manager
-
-  Everytime they fire an employee they get $100 added to their bonus.
-
-  Call your new class ProgressiveManager
-*/
-
-//Code Here
+let jason = new Manager("jason", "l", "jl@gmail.com", 24)
+console.log(jason)
 
 
+// ////////// PROBLEM 3 //////////
 
-////////// PROBLEM 4 - Black Diamond //////////
+// /*
+//   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
+//   Progressive managers have all the same properties as managers. Copy the Manager class and rename
+//   it to ProgressiveManager. Add the following additional properties:
+//     - title - default 'Not a manager'
+//     - bonus - default 0
 
-/*
-  Widget Co has a factory that makes widgets.
-  Factories have Machines.
+//   When employees are hired or fired, the manager's title should be updated based on the number of reports.
+//     0 reports : Not a manager
+//     1-3 reports : Barely Manager
+//     4-10 reports : Mostly Manager
+//     11-50 reports : Manager
+//     51-100 reports : Manager Plus
+//     101+ reports : Bestest Manager
 
-  Make a Machine class that takes in no parameters
-  A Machine has the following properties:
-    - widgets_made_count - default 0
-    - wear_and_tear_count - default 0
-    - needs_reboot - default false
+//   Everytime they fire an employee they get $100 added to their bonus.
 
-  A Machine has the following methods:
-    - makeWidgets
-        - This function takes in a number and increases widgets_made_count by that amount
-        - It also increases wear_and_tear_count by 1 for every 50
-    - fixMachine
-        - This function sets needs_reboot to true
-    - reboot
-        - This function returns an anonymous function that is called when the machine is done rebooting
-        - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
-*/
+//   Call your new class ProgressiveManager
+// */
 
-//Code Here
+// //Code Here
+
+
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age, title, bonus) {
+    super(first_name, last_name, email, age, reports)
+  
+
+
+// ////////// PROBLEM 4 - Black Diamond //////////
+
+// /*
+//   Widget Co has a factory that makes widgets.
+//   Factories have Machines.
+
+//   Make a Machine class that takes in no parameters
+//   A Machine has the following properties:
+//     - widgets_made_count - default 0
+//     - wear_and_tear_count - default 0
+//     - needs_reboot - default false
+
+//   A Machine has the following methods:
+//     - makeWidgets
+//         - This function takes in a number and increases widgets_made_count by that amount
+//         - It also increases wear_and_tear_count by 1 for every 50
+//     - fixMachine
+//         - This function sets needs_reboot to true
+//     - reboot
+//         - This function returns an anonymous function that is called when the machine is done rebooting
+//         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
+// */
+
+// //Code Here
 
 
